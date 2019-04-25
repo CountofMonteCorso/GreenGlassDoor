acea4ea11d2c7bf8ca23b9a43f0f779a2d63c6b9
Kevin Corso <kevincorso@2-XL.local>
2019-04-25 01:17:10 -0400

    a bajillion style changes + game logic functioning :]

--- index.js
+++ index.js
@@ -1,40 +1,19 @@
-//this is on the right track
-
-//take user input for their guess
-let guess = prompt("guess a word, homie").toLowerCase();
-
-//where we'll store the correct guesses
-let correctGuesses = [];
-
-//obvi
-let wrongGuesses = [];
-
-//our response to whether the item can pass
-let response = false;
-
-//function to turn guess into array to check for repeating letters
-let guessSplit = function(){
-
-//convert string to array	
-  let arr = guess.split("");
+const check = function(){ 
   
-//loop through array
-  for(let i = 0; i < arr.length; i++){
-//compare one letter with the next
-    if (arr[i] === arr[i+1]){
-//set response to true because there has been a repeating letter
-      let response = true;
-//push a successful guess into correctGuesses array
-      	if (response === true){
-     		correctGuesses.push(guess);
-      		console.log(correctGuesses);
-  		} else {
-//taunt them for incorrect guesses
-  			alert("INCORRECTAMUNDO");
-  		}
-    } 
-    
-  }
-}
+  let input = document.getElementById("input").value;
 
-guessSplit();
+  let wordArr = input.split("");
+    console.log(`ur word array is ${wordArr}`);
+
+  let doubleLetter = false;
+
+    for (let i = 0; i < wordArr.length - 1; i++){
+       if (wordArr[i] === wordArr[i+1]){
+          doubleLetter = true;
+          alert("lucky guess muthafucka");
+       }
+     }
+     if(!doubleLetter) {
+        alert("fuck dat shit try again BIOTCH"); 
+     }
+}
