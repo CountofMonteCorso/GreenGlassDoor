bb9d341dc797f22a31258c33c2070d1f7879332d
Kevin Corso <kevincorso@2-XL.local>
2019-04-25 01:36:40 -0400

    guesses columns + variables & score variable added

--- index.js
+++ index.js
@@ -1,19 +1,26 @@
 const check = function(){ 
-  
+  let score = 0;
+  let correctGuesses = [];
+  let incorrectGuesses = [];
+
   let input = document.getElementById("input").value;
 
   let wordArr = input.split("");
-    console.log(`ur word array is ${wordArr}`);
 
   let doubleLetter = false;
 
     for (let i = 0; i < wordArr.length - 1; i++){
        if (wordArr[i] === wordArr[i+1]){
           doubleLetter = true;
-          alert("lucky guess muthafucka");
+          alert("You have guessed correctly; you may pass.");
+          correctGuesses.push(input);
+          score++;
+        console.log(`score: ${score} & ${correctGuesses}`)
        }
      }
      if(!doubleLetter) {
-        alert("fuck dat shit try again BIOTCH"); 
+        alert("I will not accept this offering. Try again."); 
+        wrongGuesses.push(input);
+        console.log(`score: ${score} & ${incorrectGuesses}`)
      }
 }
